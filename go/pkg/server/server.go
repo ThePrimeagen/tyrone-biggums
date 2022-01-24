78f20923209e3542e7ad5132747e0d96ba53ad72
mpaulson <mpaulson@netflix.com>
2022-01-24 13:35:06 -0700

    feat: reduce go-funcs

--- go/pkg/server/server.go
+++ go/pkg/server/server.go
@@ -12,20 +12,46 @@ type Server struct {
 	sockets   map[uint]*Socket
 	currentId uint
 	lock      sync.Mutex
-	In        chan *Message
-	Out       chan *Message
+
+	In          chan *Message
+	Out         chan *Message
+	from_socket chan *Message
 }
 
 var upgrader = websocket.Upgrader{} // use default options
 
 func NewServer() (*Server, error) {
-	return &Server{
-		currentId: 0,
-		sockets:   make(map[uint]*Socket),
-		In:        make(chan *Message),
-		Out:       make(chan *Message),
-		lock:      sync.Mutex{},
-	}, nil
+	out := make(chan *Message)
+	from_socket := make(chan *Message)
+	server := Server{
+		currentId:   0,
+		sockets:     make(map[uint]*Socket),
+		In:          make(chan *Message),
+		from_socket: from_socket,
+		Out:         out,
+		lock:        sync.Mutex{},
+	}
+
+	go func() {
+		for {
+			select {
+			case msg := <-out:
+				server.lock.Lock()
+				server.sockets[msg.Id].Out <- msg
+				server.lock.Unlock()
+
+			case msg := <-from_socket:
+				if msg.Type == websocket.CloseMessage {
+					server.lock.Lock()
+					delete(server.sockets, msg.Id)
+					server.lock.Unlock()
+				}
+				server.In <- msg
+			}
+		}
+	}()
+
+	return &server, nil
 }
 
 func (s *Server) HandleNewConnection(w http.ResponseWriter, r *http.Request) {
@@ -33,31 +59,11 @@ func (s *Server) HandleNewConnection(w http.ResponseWriter, r *http.Request) {
 	id := s.currentId
 	s.currentId += 1
 
-    socket_channel := make(chan *Message)
-	socket, err := NewSocket(id, socket_channel, w, r)
+	socket, err := NewSocket(id, s.from_socket, w, r)
 
-    s.lock.Lock()
+	s.lock.Lock()
 	s.sockets[id] = socket
-    s.lock.Unlock()
-
-	go func() {
-		for msg := range s.Out {
-            s.lock.Lock()
-			s.sockets[msg.Id].Out <- msg
-            s.lock.Unlock()
-		}
-	}()
-
-	go func() {
-		for msg := range socket_channel {
-            if msg.Type == websocket.CloseMessage {
-                s.lock.Lock()
-                delete(s.sockets, msg.Id)
-                s.lock.Unlock()
-            }
-            s.In <- msg
-		}
-	}()
+	s.lock.Unlock()
 
 	if err != nil {
 		log.Print("couldn't upgrade socket.", err)
