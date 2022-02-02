671d37169ae296365491d6c93fb876cd2a1e8fbd
mpaulson <mpaulson@netflix.com>
2022-02-02 12:20:13 -0700

    feat: Server done

--- go/pkg/server/server.go
+++ go/pkg/server/server.go
@@ -3,71 +3,42 @@ package server
 import (
 	"log"
 	"net/http"
-	"sync"
 
 	"github.com/gorilla/websocket"
 )
 
 type Server struct {
-	sockets   map[uint]*Socket
-	currentId uint
-	lock      sync.Mutex
+	Out <-chan [2]*Socket
 
-	In          chan *Message
-	Out         chan []*Message
-	from_socket chan *Message
+	out chan [2]*Socket
+    other_socket *Socket
 }
 
 var upgrader = websocket.Upgrader{} // use default options
 
 func NewServer() (*Server, error) {
-	out := make(chan []*Message, 10000)
-	from_socket := make(chan *Message, 10000)
+	out := make(chan [2]*Socket, 10)
 	server := Server{
-		currentId:   0,
-		sockets:     make(map[uint]*Socket),
-		In:          make(chan *Message),
-		from_socket: from_socket,
-		Out:         out,
-		lock:        sync.Mutex{},
+		Out: out,
+		out: out,
+        other_socket: nil,
 	}
-
-	go func() {
-		for {
-			select {
-			case msgs := <-out:
-                count := 0
-                for _, msg := range msgs {
-                    count += 1
-                    server.sockets[msg.Id].Out <- msg
-                }
-                log.Printf("server sent %v messages", count)
-
-			case msg := <-from_socket:
-				if msg.Type == websocket.CloseMessage {
-					delete(server.sockets, msg.Id)
-				}
-				server.In <- msg
-			}
-		}
-	}()
 
 	return &server, nil
 }
 
 func (s *Server) HandleNewConnection(w http.ResponseWriter, r *http.Request) {
 
-	s.lock.Lock()
-	id := s.currentId
-	s.currentId += 1
-
-	socket, err := NewSocket(id, s.from_socket, w, r)
-
-	s.sockets[id] = socket
-	s.lock.Unlock()
+    socket, err := NewSocket(w, r)
 
 	if err != nil {
-		log.Print("couldn't upgrade socket.", err)
+		log.Fatalln("couldn't upgrade socket.", err)
 		return
 	}
+
+    if s.other_socket != nil {
+        s.out <- [2]*Socket{s.other_socket, socket}
+    } else {
+        s.other_socket = socket
+    }
 }
