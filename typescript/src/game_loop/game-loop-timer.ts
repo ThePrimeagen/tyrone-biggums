372ee90a47dfc53f5d711c715456c27239ca42c4
Ben Lesh <ben@benlesh.com>
2022-02-17 10:21:38 -0600

    refactor: rename ticker to tickerSubscription

--- typescript/src/game_loop/game-loop-timer.ts
+++ typescript/src/game_loop/game-loop-timer.ts
@@ -1,106 +1,115 @@
-import { BehaviorSubject, interval, map, Observable, Subject, Subscription, tap } from "rxjs";
+import {
+  BehaviorSubject,
+  interval,
+  map,
+  Observable,
+  Subject,
+  Subscription,
+  tap,
+} from "rxjs";
 import { explodePromise } from "../promise-helpers";
 
 type Callback = (delta: number) => void;
 
 interface StoppableTimer {
-    stop(): void;
+  stop(): void;
 }
 
 interface GameLoopTimer {
-    start(cb: Callback): void
+  start(cb: Callback): void;
 }
 
 interface GLRxJSTimer {
-    start(): Subject<number>;
+  start(): Subject<number>;
 }
 
-export default class GameLoopTimerImpl implements GameLoopTimer, StoppableTimer {
-    private tickRate: number;
-    private running: boolean;
-    private timerId?: ReturnType<typeof setTimeout>;
+export default class GameLoopTimerImpl
+  implements GameLoopTimer, StoppableTimer
+{
+  private tickRate: number;
+  private running: boolean;
+  private timerId?: ReturnType<typeof setTimeout>;
 
-    constructor(fps: number) {
-        this.tickRate = 1000 / fps;
-        this.running = false;
+  constructor(fps: number) {
+    this.tickRate = 1000 / fps;
+    this.running = false;
+  }
+
+  stop() {
+    this.running = false;
+    if (this.timerId) {
+      clearTimeout(this.timerId);
+      this.timerId = undefined;
     }
+  }
 
-    stop() {
-        this.running = false;
-        if (this.timerId) {
-            clearTimeout(this.timerId);
-            this.timerId = undefined;
-        }
-    }
+  start(cb: Callback): void {
+    this.running = true;
+    let lastTime: number = Date.now();
+    const run = () => {
+      if (!this.running) {
+        return;
+      }
 
-    start(cb: Callback): void {
-        this.running = true;
-        let lastTime: number = Date.now();
-        const run = () => {
+      const currentTime = Date.now();
+      const diff = currentTime - lastTime;
 
-            if (!this.running) {
-                return;
-            }
+      try {
+        cb(diff);
+      } catch (e) {
+        console.error(e);
+      }
 
-            const currentTime = Date.now();
-            const diff = currentTime - lastTime;
+      const opDiff = Date.now() - currentTime;
+      lastTime = currentTime;
 
-            try {
-                cb(diff);
-            } catch (e) {
-                console.error(e);
-            }
+      if (opDiff < this.tickRate) {
+        this.timerId = setTimeout(run, this.tickRate - opDiff);
+      } else {
+        setImmediate(run);
+      }
+    };
 
-            const opDiff = Date.now() - currentTime;
-            lastTime = currentTime;
-
-            if (opDiff < this.tickRate) {
-                this.timerId = setTimeout(run, this.tickRate - opDiff);
-            } else {
-                setImmediate(run);
-            }
-        }
-
-        run();
-    }
-
+    run();
+  }
 }
 
 export class GameLoopRxJS implements StoppableTimer, GLRxJSTimer {
-    private tickRate: number;
-    private ticker?: Subscription;
-    private subject: BehaviorSubject<number>;
+  private tickRate: number;
+  private tickerSubscription?: Subscription;
+  private subject: BehaviorSubject<number>;
 
-    constructor(fps: number) {
-        this.tickRate = 1000 / fps;
-        this.subject = new BehaviorSubject<number>(0);
+  constructor(fps: number) {
+    this.tickRate = 1000 / fps;
+    this.subject = new BehaviorSubject<number>(0);
+  }
+
+  stop() {
+    if (this.tickerSubscription) {
+      this.tickerSubscription.unsubscribe();
+      this.tickerSubscription = undefined;
+    }
+  }
+
+  start(): Subject<number> {
+    if (!this.tickerSubscription) {
+      let lastTime: number = Date.now();
+      this.tickerSubscription = interval(this.tickRate)
+        .pipe(
+          map(() => {
+            return Date.now() - lastTime;
+          }),
+          tap(() => {
+            lastTime = Date.now();
+          })
+        )
+        .subscribe((diff) => {
+          this.subject.next(diff);
+        });
+
+      this.subject.next(0);
     }
 
-    stop() {
-        if (this.ticker) {
-            this.ticker.unsubscribe();
-            this.ticker = undefined;
-        }
-    }
-
-    start(): Subject<number> {
-        if (!this.ticker) {
-            let lastTime: number = Date.now();
-            this.ticker = interval(this.tickRate).pipe(
-                map(() => {
-                    return Date.now() - lastTime;
-                }),
-                tap(() => {
-                    lastTime = Date.now();
-                })
-            ).subscribe((diff) => {
-                this.subject.next(diff);
-            });
-
-            this.subject.next(0);
-        }
-
-        return this.subject;
-    }
+    return this.subject;
+  }
 }
-
