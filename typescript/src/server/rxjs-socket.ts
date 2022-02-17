7c48da23eddfe3607373ddc6eb01b134eafd7c5e
Ben Lesh <ben@benlesh.com>
2022-02-17 13:45:45 -0600

    refactor: Move socket observable creation to its own function for later testing.

--- typescript/src/server/rxjs-socket.ts
+++ typescript/src/server/rxjs-socket.ts
@@ -3,27 +3,30 @@ import WebSocket, { RawData } from "ws";
 import { Message } from "../message";
 import { BaseSocket, RxSocket } from "./universal-types";
 
+export function fromWebSocket(socket: WebSocket): Observable<Message> {
+  return new Observable((subscriber) => {
+    const messageHandler = (msg: RawData) =>
+      subscriber.next(JSON.parse(msg.toString()));
+    socket.on("message", messageHandler);
+
+    const closeHandler = () => subscriber.complete();
+    socket.on("close", closeHandler);
+
+    const errorHandler = (e: Error) => subscriber.error(e);
+    socket.on("error", errorHandler);
+
+    return () => {
+      socket.off("message", messageHandler);
+      socket.off("close", closeHandler);
+      socket.off("error", errorHandler);
+    };
+  });
+}
 export default class SocketImpl implements RxSocket, BaseSocket {
   readonly events: Observable<Message>;
 
   constructor(private socket: WebSocket) {
-    this.events = new Observable((subscriber) => {
-      const messageHandler = (msg: RawData) =>
-        subscriber.next(JSON.parse(msg.toString()));
-      this.socket.on("message", messageHandler);
-
-      const closeHandler = () => subscriber.complete();
-      this.socket.on("close", closeHandler);
-
-      const errorHandler = (e: Error) => subscriber.error(e);
-      this.socket.on("error", errorHandler);
-
-      return () => {
-        this.socket.off("message", messageHandler);
-        this.socket.off("close", closeHandler);
-        this.socket.off("error", errorHandler);
-      };
-    });
+    this.events = fromWebSocket(socket);
   }
 
   close(code?: number): void {
