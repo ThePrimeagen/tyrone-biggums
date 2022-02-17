8c8ee6495b3d6b313d4cef5eda96cb36c0392343
Ben Lesh <ben@benlesh.com>
2022-02-17 09:06:51 -0600

    refactor: Use a promise where you just wanted a promise.

--- typescript/src/server/rxjs-server.ts
+++ typescript/src/server/rxjs-server.ts
@@ -1,81 +1,84 @@
 import WebSocket from "ws";
 import Socket from "./rxjs-socket";
-import { BehaviorSubject, filter, map, Observable, Observer, scan, Subject } from "rxjs";
+import {
+  BehaviorSubject,
+  filter,
+  map,
+  Observable,
+  Observer,
+  scan,
+  Subject,
+} from "rxjs";
+import { once } from "events";
 
 export interface Server {
-    listening: BehaviorSubject<boolean>;
-    close(): void;
-    on(): Observable<[Socket, Socket]>;
+  listening: Promise<any>;
+  close(): void;
+  on(): Observable<[Socket, Socket]>;
 }
 
 export default class ServerImpl implements Server {
-    private subject: Subject<[Socket, Socket]>;
-    public listening: BehaviorSubject<boolean>;
-    private server: WebSocket.Server;
+  private subject: Subject<[Socket, Socket]>;
+  public readonly listening: Promise<any>;
+  private server: WebSocket.Server;
 
-    constructor(port: number = 42069) {
-        this.subject = new Subject<[Socket, Socket]>();
-        this.listening = new BehaviorSubject<boolean>(false);
-        this.server = new WebSocket.Server({
-            host: "0.0.0.0",
-            port,
+  constructor(port: number = 42069) {
+    this.subject = new Subject<[Socket, Socket]>();
+    this.server = new WebSocket.Server({
+      host: "0.0.0.0",
+      port,
+    });
+    this.listening = once(this.server, "listening");
+
+    this.startServer(this.server);
+  }
+
+  close(): void {
+    this.subject.complete();
+    this.server.close();
+  }
+
+  public on(): Observable<[Socket, Socket]> {
+    return this.subject;
+  }
+
+  private startServer(server: WebSocket.Server) {
+    this.server = server;
+    const observable: Observable<WebSocket> = Observable.create(
+      (observer: Observer<WebSocket>) => {
+        server.on("connection", (ws) => {
+          observer.next(ws);
         });
-        this.startServer(this.server);
-    }
+      }
+    );
 
-    close(): void {
-        this.subject.complete();
-        this.server.close();
-    }
+    observable
+      .pipe(
+        scan((group: WebSocket[], ws: WebSocket) => {
+          if (!group || group.length === 2) {
+            group = [];
+          }
 
-    public on(): Observable<[Socket, Socket]> {
-        return this.subject;
-    }
+          group.push(ws);
+          return group;
+        }, []),
+        filter((group: WebSocket[]) => {
+          return group.length === 2;
+        }),
+        map<WebSocket[], [Socket, Socket]>((group: WebSocket[]) => {
+          return [new Socket(group[0]), new Socket(group[1])];
+        })
+      )
+      .subscribe((socketGroup: [Socket, Socket]) => {
+        this.subject.next(socketGroup);
+      });
 
-    private startServer(server: WebSocket.Server) {
-        this.server = server;
-        const observable: Observable<WebSocket> = Observable.create((observer: Observer<WebSocket>) => {
-            server.on("connection", ws => {
-                observer.next(ws);
-            });
-        });
+    server.on("error", (e) => {
+      this.subject.error(e);
+    });
 
-        observable.pipe(
-            scan((group: WebSocket[], ws: WebSocket) => {
-                if (!group || group.length === 2) {
-                    group = [];
-                }
-
-                group.push(ws);
-                return group;
-            }, []),
-            filter((group: WebSocket[]) => {
-                return group.length === 2;
-            }),
-            map<WebSocket[], [Socket, Socket]>((group: WebSocket[]) => {
-                return [
-                    new Socket(group[0]),
-                    new Socket(group[1]),
-                ];
-            })
-        ).subscribe((socketGroup: [Socket, Socket]) => {
-            this.subject.next(socketGroup);
-        });
-
-        server.on("error", e => {
-            this.subject.error(e);
-        });
-
-        server.on("close", () => {
-            this.subject.complete();
-        });
-
-        server.on("listening", (e?: Error) => {
-            if (e) {
-                this.listening.error(e);
-            } else {
-                this.listening.next(true);
-            }
-        });
-    }
+    server.on("close", () => {
+      this.subject.complete();
+    });
+  }
 }
