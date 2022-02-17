057a7dce8e27224b7354c80badac429fed6198df
Ben Lesh <ben@benlesh.com>
2022-02-17 09:26:40 -0600

    perf: Use Observable instead of unnecessary Subject.

--- typescript/src/server/rxjs-socket.ts
+++ typescript/src/server/rxjs-socket.ts
@@ -1,34 +1,32 @@
-import { Subject } from "rxjs";
+import { Observable, Subject } from "rxjs";
 import WebSocket from "ws";
 import { createMessage, Message } from "../message";
 import { BaseSocket, RxSocket } from "./universal-types";
 
 export default class SocketImpl implements RxSocket, BaseSocket {
-    events: Subject<Message>;
+  readonly events: Observable<Message>;
 
-    constructor(private socket: WebSocket) {
-        this.events = new Subject<Message>();
+  constructor(private socket: WebSocket) {
+    this.events = new Observable((subscriber) => {
+      this.socket.on("message", (msg) => {
+        subscriber.next(JSON.parse(msg.toString()));
+      });
 
-        this.socket.on("message", (msg) => {
-            this.events.next(JSON.parse(msg.toString()));
-        });
+      this.socket.on("close", () => {
+        subscriber.complete();
+      });
 
-        this.socket.on("close", () => {
-            this.events.complete();
-        });
+      this.socket.on("error", (e: Error) => {
+        subscriber.error(e);
+      });
+    });
+  }
 
-        this.socket.on("error", (e: Error) => {
-            this.events.error(e);
-        });
-    }
+  close(code?: number): void {
+    this.socket.close(code);
+  }
 
-    close(code?: number): void {
-        this.socket.close(code);
-    }
-
-    push(data: object, cb?: () => void): void {
-        this.socket.send(JSON.stringify(data), cb);
-    }
+  push(data: object, cb?: () => void): void {
+    this.socket.send(JSON.stringify(data), cb);
+  }
 }
-
-
--- typescript/src/server/universal-types.ts
+++ typescript/src/server/universal-types.ts
@@ -1,24 +1,23 @@
-import { Subject } from "rxjs";
+import { Observable } from "rxjs";
 import { Message } from "../message";
 
 export interface BaseSocket {
-    // Events
-    push(data: object, cb?: () => void): void;
-    close(code?: number): void;
+  // Events
+  push(data: object, cb?: () => void): void;
+  close(code?: number): void;
 }
 
 export interface CallbackSocket {
-    close(code?: number): void;
-    push(data: object, cb?: () => void): void;
-    onclose?: () => void,
-    onmessage?: (message: Message) => void,
-    onerror?: (e: Error) => void,
-    clean(): void;
+  close(code?: number): void;
+  push(data: object, cb?: () => void): void;
+  onclose?: () => void;
+  onmessage?: (message: Message) => void;
+  onerror?: (e: Error) => void;
+  clean(): void;
 }
 
 export interface RxSocket {
-    close(code?: number): void;
-    push(data: object, cb?: () => void): void;
-    events: Subject<Message>;
+  close(code?: number): void;
+  push(data: object, cb?: () => void): void;
+  events: Observable<Message>;
 }
-
