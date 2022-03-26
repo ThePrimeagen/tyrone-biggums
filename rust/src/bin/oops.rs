1ddb63fac0a26c6c37ec39ceb7b6175b127fdcbc
mpaulson <mpaulson@netflix.com>
2022-03-26 07:34:21 -0600

    feat: runner

--- rust/src/bin/oops.rs
+++ rust/src/bin/oops.rs
@@ -1,22 +1,4 @@
-use futures::StreamExt;
-
-#[tokio::main]
-async fn main() {
-    let url = url::Url::parse("ws://0.0.0.0:42069").unwrap();
-
-    println!("About to handshake this little connection");
-    let (ws_stream, _) = tokio_tungstenite::connect_async(url).await.expect("Failed to connect");
-    println!("WebSocket handshake has been successfully completed");
-
-    let (_, mut read) = ws_stream.split();
-
-    if let Some(Ok(item)) = read.next().await {
-        if item.is_text() {
-            println!("Text, but still, what the hell? {:?}", item);
-        } else {
-            println!("what the hell did i just get ?? {:?}", item);
-        }
-    } else {
-        println!("I tried to read next and got something weird");
-    }
+fn main() {
+    let value: u64 = (1 << 32) - 1u64;
+    println!("{}", value);
 }
